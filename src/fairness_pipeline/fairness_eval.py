7457d144ed197588fc8f33fb7776268e8b31f977
Shubin Li <shubin.li2@mail.dcu.ie>
2026-06-17 00:36:31 +0100

    add gitignore and confirm the fairness matrics use

--- src/fairness_pipeline/fairness_eval.py
+++ src/fairness_pipeline/fairness_eval.py
@@ -3,4 +3,14 @@ Shubin Li
 
 public fairness evaluation as interface for both nhanes and oulad
 
+fairness metrics ：
+Equalized Odds
+Equal Opportunity
+Demographic Parity
+
+
 """
+
+
+# test? 数据按 敏感属性 分组  然后利用公平性工具 看看分数是否不同
+# 
